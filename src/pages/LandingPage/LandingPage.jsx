34f4a43a0b406de9329e020f45a16387bed8cbe3
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-08-31 09:38:22 +0200

    landing_page: fixed link to scroll

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,25 @@
 import { Link } from "react-router-dom";
 import roomBackground from "../../assets/landingpage/room_background.png"
 import { About, Footer, Navbar, Product } from "./component";
+import { useRef } from "react";
 
 export function LandingPage() {
+
+    const homeRef = useRef(null);
+    const productRef = useRef(null);
+    const aboutRef = useRef(null);
+    const contactRef = useRef(null);
+
   return (
     <>
     <main className="h-[728px] bg-orange-400">
-        <Navbar />
-        <section id="home" className="relative max-w-screen-xl flex items-center mx-auto h-[calc(728px_-_12.5rem)]">
+        <Navbar
+          homeRef={homeRef}
+          productRef={productRef}
+          aboutRef={aboutRef}
+          contactRef={contactRef}
+        />
+        <section className="relative max-w-screen-xl flex items-center mx-auto h-[calc(728px_-_12.5rem)]">
           <div className="w-2/5 flex flex-col justify-center text-white gap-4">
             <h1 className="text-9xl font-neurial-extrabold">Room</h1>
             <h2 className="text-4xl font-bold tracking-wide">Connect in Your Space</h2>
@@ -16,18 +28,18 @@ export function LandingPage() {
             <button className="w-48 py-4 mt-4 bg-blue-500 font-light tracking-widest rounded">LOG IN</button>
             </Link>
           </div>
-          <div className="w-3/5">
+          <div className="w-3/5 -z-1 pointer-events-none">
             <img className="relative size-[40rem] top-16 left-32" src={roomBackground} alt="Room Background" />
           </div>
         </section>
     </main>
-    <section id="product" className="max-w-screen-xl mx-auto py-28">
+    <section ref={productRef} className="max-w-screen-xl mx-auto py-28">
       <Product />
     </section>
-    <section id="about" className="bg-orange-400">
+    <section ref={aboutRef} className="bg-orange-400">
       <About />
     </section>
-    <Footer />
+    <Footer contactRef={contactRef} />
     </>
   )
 }
